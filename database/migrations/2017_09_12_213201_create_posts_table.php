529f65a06ea71b661e390a35214c94a462fe9e5a
Satya Kresna <satyakresna6295@gmail.com>
2017-09-13 20:27:33 +0800

    Sesi I : Setup table dan authentikasi

--- database/migrations/2017_09_12_213201_create_posts_table.php
+++ database/migrations/2017_09_12_213201_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        // Related issue: https://github.com/laravel/framework/issues/17508
-        Schema::defaultStringLength(191);
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('category_id')->unsigned();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('CASCADE');
         });
     }
 
@@ -32,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
